a10cfd1dec22c9844542921e22342ff52f8e58a8
John Asaro <johnnyasaro63@gmail.com>
2025-06-25 16:46:33 -0400

    fixed some command line stuff

--- external/script/main.lua
+++ external/script/main.lua
@@ -357,8 +357,8 @@ end
 if main.flags['-speedtest'] ~= nil then
 	setGameSpeed(100 * gameOption('Config.Framerate'))
 end
-if main.flags['-nosound'] ~= nil then
-	setVolumeMaster(0)
+if main.flags['-nosound'] ~= nil then -- Does not work, use setvolume flag
+	modifyGameOption('Sound.MasterVolume', 0)
 end
 if main.flags['-togglelifebars'] ~= nil then
 	toggleLifebarDisplay()
@@ -372,6 +372,17 @@ end
 if main.flags['-setport'] ~= nil then
 	setListenPort(main.flags['-setport'])
 end
+if main.flags['-setvolume'] ~= nil and main.flags['-nosound'] == nil then
+	modifyGameOption('Sound.MasterVolume', main.flags['-setvolume'])
+end
+if main.flags['-windowed'] ~= nil then
+	modifyGameOption('Video.Fullscreen', false)
+end
+if main.flags['-windowed'] ~= nil and main.flags['-width'] ~= nil and main.flags['-height'] ~= nil then
+	modifyGameOption('Video.GameWidth', main.flags['-width'])
+	modifyGameOption('Video.GameHeight', main.flags['-height'])
+end
+
 
 --motif
 main.motifDef = gameOption('Config.Motif')
